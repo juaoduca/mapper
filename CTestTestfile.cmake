312eb6f40aa359bb3d775be374d4f7633051e17e
juaoduca <master@NOTE-DELL.>
2025-08-12 15:25:43 -0300

    adding publish-snapshot.yml

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/master/dev/lib/mapper
+# Build directory: /home/master/dev/lib/mapper
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(OrmSchemaManagerTests "/home/master/dev/lib/mapper/tests")
+set_tests_properties(OrmSchemaManagerTests PROPERTIES  _BACKTRACE_TRIPLES "/home/master/dev/lib/mapper/CMakeLists.txt;19;add_test;/home/master/dev/lib/mapper/CMakeLists.txt;0;")
